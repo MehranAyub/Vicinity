20e7082b7d8699d12a852657851b110aa8a60913
Faisal Ayub <fayub@payactiv.com.pk>
2021-11-01 07:38:20 -0700

    mapMarker implement simple

---
+++ MCN.Angular/ClientApp/src/app/modules/job/models/mapService.ts
@@ -0,0 +1,35 @@
+export interface Marker{
+    position:Position;
+    title:string;
+    option:Option;
+    info:string;
+    label:Label;
+}
+
+export interface Position{
+    lat:number;
+    lng:number;
+}
+
+export interface Option{
+animation:any;
+icon:any
+}
+export interface Label{
+    color:any;
+    text:any;
+}
+
+// this.markers.push({
+//     position: {
+//       lat: item.lat,
+//       lng: item.long,
+//     },
+//     title: item.firstName+item.lastName,
+//     options: {
+//       animation: null,
+//       icon: {
+//         url: 'assets/Images/icons/PickPin.svg',
+//       },
+//     },
+//   });
--- MCN.Angular/ClientApp/src/app/modules/job/pages/home/home.component.ts
+++ MCN.Angular/ClientApp/src/app/modules/job/pages/home/home.component.ts
@@ -1,6 +1,23 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { Subscription } from 'rxjs';
+import { Marker } from '../../models/mapService';
 declare var $:any;
+
+const Marker = {
+  position: {
+    lat: 37.3382,
+    lng: -121.8863,
+  },
+  title: '',
+  options: {
+    animation: google.maps.Animation.DROP,
+    icon: {
+      url: 'assets/img/PickPin.svg',
+    },
+  },
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,6 +26,8 @@ declare var $:any;
 export class HomeComponent implements OnInit {
 
   // @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false }) info: any;
+  
   center: google.maps.LatLngLiteral = { lat: 37.3382, lng: -121.8863 };
   zoom = 12;
   options: google.maps.MapOptions = {
@@ -23,7 +42,8 @@ export class HomeComponent implements OnInit {
     fullscreenControl: false,
     center: new google.maps.LatLng(37.3382, -121.8863),
   };
-  public markers = [];
+  public markers:Marker[] = [];
+  public latLong=[{firstName:'Niazi',lastName:'Town',lat:33.599239344482179,long:73.009821019136709},{firstName:'Misrial',lastName:'Chowk',lat:33.602050353186954,long:72.98370354939469}];
 
   imageUrl = 'https://angular.io/assets/images/logos/angular/angular.svg';
   imageBounds: google.maps.LatLngBoundsLiteral = {
@@ -34,7 +54,10 @@ export class HomeComponent implements OnInit {
   };
   vertices: google.maps.LatLngLiteral[] = [];
   
-  constructor() { }
+  constructor() { 
+
+    
+  }
 
   ngOnInit(): void {
     $(document).ready(function(){
@@ -48,6 +71,47 @@ export class HomeComponent implements OnInit {
        return false;
    });
    });
+
+
+   navigator.geolocation.getCurrentPosition((position) => {
+    this.center = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    }
+  });
+  
+   this.latLong.forEach((item)=>{
+
+    this.markers.push({
+      position: {
+        lat: item.lat,
+        lng: item.long,
+      },
+      title:'Marker title '+ item.firstName+item.lastName,
+      label: {
+        color: 'red',
+        text: 'Marker label ' + (this.markers.length + 1),
+      },
+      info: 'Marker info ' + (this.markers.length + 1),
+
+      option: {
+        animation: google.maps.Animation.DROP,
+        icon: {
+          url: 'assets/img/PickPin.svg',
+        },
+      },
+    });
+   })
+
+
+
+  }
+
+  infoContent = ''
+  openInfo(marker: any) {
+    console.log(marker);
+    this.infoContent = "Faisal Ayub here <br/> ABCEFBERIV"
+    this.info.open(marker);
   }
 
 }
