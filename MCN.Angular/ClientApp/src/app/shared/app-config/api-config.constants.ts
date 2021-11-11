b1a366366d585d5ad560bff159cf763613869963
Faisal Ayub <fayub@payactiv.com.pk>
2021-11-10 16:22:47 -0800

    checkin

--- MCN.Angular/ClientApp/src/app/shared/app-config/api-config.constants.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { IApiConfig } from './api-config.interface';
-
-export const API_DI_CONFIG: IApiConfig = {
-  JWTKeyName: 'jwtToken'
-}
-export let API_CONFIG = new InjectionToken<IApiConfig>('api.config');
