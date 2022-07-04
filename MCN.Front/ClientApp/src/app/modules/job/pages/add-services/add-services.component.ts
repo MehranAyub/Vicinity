fd2fa5d5b662138b922882936c557ecb4fbae3c6
Mehran Ayub <mehranayub3@gmail.com>
2022-07-04 16:41:21 +0500

    Final commits

--- MCN.Front/ClientApp/src/app/modules/job/pages/add-services/add-services.component.ts
+++ MCN.Front/ClientApp/src/app/modules/job/pages/add-services/add-services.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonComponent } from 'src/app/shared/components/common/common.component';
 import { AnimationService, AnimationType } from 'src/app/shared/services/animation/animation.service';
+import { NotificationTypeEnum, SnackBarService } from 'src/app/shared/snack-bar.service';
 import { UserInterestDto } from '../../models/User';
 import { JobService } from '../../service/jobService';
 
@@ -14,7 +15,7 @@ export class AddServicesComponent extends CommonComponent implements OnInit {
 userId:number=null;
 Services:any;
 interestDto:UserInterestDto={interestId:null,userId:null}
-  constructor(_router:Router,animationService:AnimationService,private jobService:JobService) {
+  constructor(_router:Router,animationService:AnimationService,private jobService:JobService,private _snackbarService:SnackBarService) {
     super(_router,animationService);
     let user=JSON.parse(localStorage.getItem('currentUser'));
    this.userId=user.user.id;
@@ -38,7 +39,7 @@ AddService(id){
   this.interestDto.interestId=id;
   this.jobService.AddService(this.interestDto).subscribe((res)=>{
     if(res){
-      console.log(res.data);
+      this._snackbarService.openSnack("Service added Successfully",NotificationTypeEnum.Success); 
    
   }})
   }
--- MCN.Front/ClientApp/src/app/modules/job/pages/my-services/my-services.component.ts
+++ MCN.Front/ClientApp/src/app/modules/job/pages/my-services/my-services.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonComponent } from 'src/app/shared/components/common/common.component';
 import { AnimationService, AnimationType } from 'src/app/shared/services/animation/animation.service';
+import { NotificationTypeEnum, SnackBarService } from 'src/app/shared/snack-bar.service';
 import { UserInterestDto } from '../../models/User';
 import { JobService } from '../../service/jobService';
 
@@ -14,7 +15,7 @@ export class MyServicesComponent extends CommonComponent implements OnInit {
   userId:number=null;
   Services:any;
   interestDto:UserInterestDto={interestId:null,userId:null}
-    constructor(_router:Router,animationService:AnimationService,private jobService:JobService) {
+    constructor(_router:Router,animationService:AnimationService,private jobService:JobService,private _snackbarService:SnackBarService) {
       super(_router,animationService);
       let user=JSON.parse(localStorage.getItem('currentUser'));
      this.userId=user.user.id;
@@ -36,6 +37,7 @@ export class MyServicesComponent extends CommonComponent implements OnInit {
   RemoveService(id){
     this.interestDto.interestId=id;
     this.jobService.RemoveService(this.interestDto).subscribe((res)=>{
+      this._snackbarService.openSnack("Service Removed Successfully",NotificationTypeEnum.Warning); 
      this.ngOnInit();
   })
 }  
